22bd59e41c9d3f87c3b2a7df081bd1d1024dec1f
ramlabserver <yliuhb@connect.ust.hk>
2023-09-21 02:45:44 +0000

    fix a bug in rotation prediction

--- mono3d/model/head.py
+++ mono3d/model/head.py
@@ -438,7 +438,7 @@ class MonoFlexHead(nn.Module):
         # keypoint L1 loss
         hp_loss = self._RegWeightedL1Loss(output['hps'],annotations['hps_mask'], annotations['ind'], annotations['hps'],annotations['dep'].clone())
         # rotations from RTM3D
-        rot_loss = self._RotLoss(output['rot'], annotations['reg_mask'], annotations['ind'], annotations['rotbin'], annotations['rotres'])
+        rot_loss = self._RotLoss(output['rot'], annotations['3D_reg_mask'], annotations['ind'], annotations['rotbin'], annotations['rotres'])
 
         # gather output
         gathered_output = self._gather_output(output, annotations['ind'], annotations['3D_reg_mask'], annotations['reg_mask'])
